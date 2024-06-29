3363c4873f89d27d9414dd54b96c510a36f10cc3
ahmed.shan <ahmedshan16@gmail.com>
2024-06-30 02:58:56 +0500

    Added final coding

--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/joho/godotenv"
 )
 
-
+var parentProjectDirectory string
 
 func main() {
 
@@ -18,26 +18,60 @@ func main() {
 
 	fmt.Println("Wow! New Project")
 
-	fmt.Println(getEnv("PROJECT_DIR"))
-
 	// Get the required details
+	clientName := askForInput("Client Name: ")
+	clientEmail := askForInput("Client Email: ")
+	clientContact := askForInput("Client Contact: ")
 	projectName := askForInput("Enter the project name: ")
+	projectDate := askForInput("Enter Project Date (YYYY-MM-DD): ")
+
+	parentProjectDirectory = getProjectDir() + "/" + projectName
+
+	// First create the current project directory
+	os.Mkdir(getProjectDir(), 0755)
+	os.Mkdir(parentProjectDirectory, 0755)
+
+	createProjectDirectories()
+
+	createIndexFile(clientName, clientEmail, clientContact, projectName, projectDate)
 
 	fmt.Println("Project Name: " + projectName)
+	fmt.Println("Project Directory: " + parentProjectDirectory)
 
 }
 
-func getEnv(value string) string {
-	err := godotenv.Load(".env")
+func getProjectDir() string {
+
+	environmentValue, err := getEnv("PROJECT_DIR")
 	if err != nil {
-	  log.Fatalf("Error loading .env file")
+		homeDir, _ := os.UserHomeDir()
+		return homeDir
 	}
-	return os.Getenv(value)
+	return environmentValue
+}
+
+func getEnv(key string) (value string, err error) {
+
+	f, _ := os.Getwd()
+	projectDirectory := filepath.Dir(f) + "/" + filepath.Base(f)
+	environmentFile := projectDirectory + "/.env"
+
+	// Check if the environment file exist
+	_, err = os.Stat(environmentFile)
+	if err != nil {
+		return
+	}
+	err = godotenv.Load(environmentFile)
+	if err != nil {
+		return
+	}
+	value = os.Getenv(key)
+	return
 }
 
 func askForInput(question string) string {
 	var s string
-	r:=bufio.NewReader(os.Stdin)
+	r := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Fprint(os.Stderr, question+" ")
 		s, _ = r.ReadString('\n')
@@ -46,4 +80,29 @@ func askForInput(question string) string {
 		}
 	}
 	return strings.TrimSpace(s)
+}
+
+func createProjectDirectories() (response bool, err error) {
+
+	subDirectoryNames := [5]string{"RAW Files", "Music", "Assets", "Project Files", "Deliverables"}
+
+	for _, folder := range subDirectoryNames {
+		actualDirectory := parentProjectDirectory + "/" + folder
+		os.Mkdir(actualDirectory, 0755)
+	}
+
+	response = true
+
+	return
+}
+
+func createIndexFile(clientName string, clientEmail string, clientContact string, projectName string, projectDate string) {
+	inputText := "Client Name: " + clientName + "\n"
+	inputText = inputText + "Client Email: " + clientEmail + "\n"
+	inputText = inputText + "Client Contact: " + clientContact + "\n"
+	inputText = inputText + "Project Name: " + projectName + "\n"
+	inputText = inputText + "Project Date: " + projectDate
+
+	os.WriteFile(parentProjectDirectory+"/readme.txt", []byte(inputText), 0644)
+
 }
